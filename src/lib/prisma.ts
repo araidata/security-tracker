ae23012ffa5277100c76e1ba0a40017b460a6dc5
Aaron <aaronaidata@gmail.com>
2026-04-08 23:47:06 -0500

    Use unpooled Prisma URL in production

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -4,9 +4,15 @@ const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined;
 };
 
+const datasourceUrl =
+  process.env.NODE_ENV === "production"
+    ? process.env.DATABASE_URL_UNPOOLED ?? process.env.DATABASE_URL
+    : process.env.DATABASE_URL;
+
 export const prisma =
   globalForPrisma.prisma ??
   new PrismaClient({
+    datasourceUrl,
     log: process.env.NODE_ENV === "development" ? ["query"] : [],
   });
 
